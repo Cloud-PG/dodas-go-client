a8b79405776305865fa3e02fd62db2423cb045b5
dciangot <diego.ciangottini@gmail.com>
2019-12-12 12:45:43 +0100

    fix message

--- cmd/get.go
+++ cmd/get.go
@@ -75,7 +75,7 @@ var outputCmd = &cobra.Command{
 
 		body, _ := ioutil.ReadAll(resp.Body)
 
-		fmt.Print("Deployment status:\n")
+		fmt.Print("Deployment output:\n")
 
 		if resp.StatusCode == 200 {
 			fmt.Println(string(body))
